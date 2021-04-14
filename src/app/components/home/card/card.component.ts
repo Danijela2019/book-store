f20a3d59d8713a41fb4538c1eb25254156270263
Danijela2019 <arsic_danijela@yahoo.com>
2021-04-14 13:56:57 +0200

    Added authentication guard

--- src/app/components/home/card/card.component.ts
+++ src/app/components/home/card/card.component.ts
@@ -1,24 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Book } from 'src/app/models/Book';
 import { WishlistService } from 'src/app/services/wishlist.service';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, OnDestroy {
   @Input() book: Book;
   addedToFavorites: boolean = false;
   text: string;
-  constructor(private wishlistService: WishlistService) {}
+  private userSubscription: Subscription;
+  isAuthenticated = false;
 
-  ngOnInit(): void {}
+  constructor(
+    private wishlistService: WishlistService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.userSubscription = this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !user ? false : true;
+    });
+  }
 
   onHeartClicked(clickedBook: Book) {
     this.wishlistService
       .addBooksToWishList(clickedBook)
       .subscribe((_res) => (this.text = 'Successfully added'));
     this.addedToFavorites = true;
+  }
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
   }
 }
